ee67876c7169d9c5fc26ab8bf2ea1f738ad551e5
Aeryan <raunoita@gmail.com>
2021-03-09 12:55:04 +0200

    Logging proxy

---
+++ auxiliary/logger.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import socket
+import re
+import os
+import ast
+
+# https://docs.python.org/3/howto/sockets.html
+nginx_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+nginx_socket.bind(("localhost", 5005))
+
+nginx_socket.listen(5)
+while True:
+    # Receive and parse data from the Nginx server
+    (clientsocket, address) = nginx_socket.accept()
+    data_in = clientsocket.recv(4096)
+    msg_data_in = ast.literal_eval(re.findall(r"{[\s\S]+}", data_in.decode("utf-8"))[0])
+
+    # Forward data to Rasa server
+    rasa_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    rasa_socket.connect(("localhost", 5006))
+    rasa_socket.send(data_in)
+
+    # Log the incoming data
+    with open(os.path.join("logs", str(msg_data_in['sender']) + ".txt"), 'a') as file:
+        file.write("User: " + msg_data_in['message'] + "\n")
+    file.close()
+
+    # Receive and parse data from the Rasa server, close the client socket (crashes otherwise), and forward the data
+    data_out = rasa_socket.recv(4096)
+    msg_data_out = ast.literal_eval(re.findall(r"{[\s\S]+}", data_out.decode("utf-8"))[0])
+    rasa_socket.close()
+    clientsocket.send(data_out)
+
+    # If outgoing data has only one message, the data structure needs to be modified
+    if type(msg_data_out) != tuple:
+        msg_data_out = (msg_data_out,)
+    # Log the outgoing message(s)
+    with open(os.path.join("logs", str(msg_data_out[0]['recipient_id']) + ".txt"), 'a') as file:
+        for msg in msg_data_out:
+            file.write("Bot: " + msg['text'] + "\n")
+    file.close()

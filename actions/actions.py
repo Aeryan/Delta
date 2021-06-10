acd95331b20fd12db06c760993bbaf06cd50d995
Aeryan <raunoita@gmail.com>
2021-06-10 12:44:20 +0300

    Nädalapäevade indekseerimise algus 0->1

--- actions/actions.py
+++ actions/actions.py
@@ -16,7 +16,7 @@ DATABASE_USER = "postgres"
 DATABASE_PASSWORD = "postgres"
 
 # Nädalapäevade nimetused
-WEEKDAYS = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+WEEKDAYS = {1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday", 7: "Sunday"}
 
 
 class ActionResetAllSlots(Action):
